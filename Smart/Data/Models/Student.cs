189ce67d6cfe2c74d9e66b56b5f718d1735f089e
krazykopter <carsoncrezee@gmail.com>
2019-10-30 21:19:02 -0600

    Created basic  & started styling CRUD for Applications. Also added Fontawesome

--- Smart/Data/Models/Student.cs
+++ Smart/Data/Models/Student.cs
@@ -11,29 +11,41 @@ namespace Smart.Data.Models
     public class Student
     {
         public int StudentId { get; set; }
-        [Required]
-        [MaxLength(32)]
+
+        [Required, MaxLength(32), Display(Name = "First Name")]
         public string FirstName { get; set; }
-        [Required]
-        [MaxLength(32)]
+
+        [Required, MaxLength(32) ,Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [DataType(DataType.Date)]
+
+        [Required, DataType(DataType.Date), Display(Name = "Date of Birth")]
         public DateTime? DateOfBirth { get; set; }
+
         [MaxLength(128)]
         public string Address { get; set; }
+
         [MaxLength(64)]
         public string Village { get; set; }
+
+        [Required, Display(Name = "Lattitude")]
         public double LocationLattitude { get; set; }
+
+        [Required, Display(Name = "Longitude")]
         public double LocationLongitude { get; set; }
+
+        [Required, Display(Name = "Public School Level")]
         public byte PublicSchoolLevel { get; set; }
-        [MaxLength(64)]
+
+        [Required, MaxLength(64), Display(Name = "Guardian Name")]
         public string GuardianName { get; set; }
-        [MaxLength(16)]
+
+        [MaxLength(16), Display(Name = "Phone Number")]
         public string Phone { get; set; }
+
         [MaxLength(256)]
         public string Photo { get; set; }
-        public StudentStatusEnum StudentStatusId { get; set; }
 
+        public StudentStatusEnum StudentStatusId { get; set; }
 
         public virtual StudentStatus StudentStatus { get; set; }
 
---
+++ Smart/Pages/Application/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Smart.Data;
+using Smart.Data.Models;
+
+namespace Smart.Pages.Application
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Smart.Data.ApplicationDbContext _context;
+
+        public IndexModel(Smart.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Students
+                .Include(s => s.StudentStatus).ToListAsync();
+        }
+    }
+}
