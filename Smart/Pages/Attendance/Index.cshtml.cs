d3afa7fb4525a1e86fa8aff1a75b326bc44cbd3d
manal alsharif <manalalsharif@mail.weber.edu>
2019-11-02 19:37:54 -0600

    Adding Attendance Index page

---
+++ Smart/Pages/Attendance/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.Pages.Attendance
+{
+    public class IndexModel : PageModel
+
+        
+    {
+        public int AttendanceFile { get;}
+
+        public void OnGet()
+        {
+
+           
+
+        }
+    }
+}
