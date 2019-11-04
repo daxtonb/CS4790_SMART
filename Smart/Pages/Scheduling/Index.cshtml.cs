296e2107357cf3c5d53436a207505702ac4392ac
Austin Wilcox <austin21wilcox@gmail.com>
2019-11-04 09:20:00 -0700

    Edited the scheduling, and the events and errors page. Which is now pulling from the server.

--- Smart/Pages/Scheduling/Index.cshtml.cs
+++ Smart/Pages/Scheduling/Index.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Smart.Data;
 
 namespace MyApp.Namespace
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public List<string> DaysOfTheWeek {get; set;} = new List<string>(){"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        
+        public IndexModel(ApplicationDbContext dbContext)
         {
+            _db = dbContext;
+        }
+
+        public async Task OnGetAsync(int? studentId)
+        {
+            
         }
     }
 }
