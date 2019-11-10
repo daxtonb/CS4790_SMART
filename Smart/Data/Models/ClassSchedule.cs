6edf0bc51ae15a60fcfb1f5b675eb4e1ca83707c
Daxton Burgon <dax@dentma.com>
2019-11-10 00:18:56 -0700

    Added class details page.

--- Smart/Data/Models/ClassSchedule.cs
+++ Smart/Data/Models/ClassSchedule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Smart.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -14,5 +15,38 @@ namespace Smart.Data.Models
 
         public virtual Class Class { get; set; }
         public virtual ScheduleAvailability ScheduleAvailability { get; set; }
+        public static string GetScheduleString(IEnumerable<ClassSchedule> classSchedules)
+        {
+            string dayOfWeek, timeRange, workignString = string.Empty;
+            ScheduleAvailability current, next = null;
+            var arr = classSchedules.ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                current = arr[i].ScheduleAvailability;
+                next = i < arr.Length - 1 ? arr[i + 1].ScheduleAvailability : null;
+                workignString += GetDayOfWeekAbbreviation(current.DayOfWeek);
+
+                // CONDITION: This is the last element or the next element has a different time schedule
+                if (i == arr.Length - 1 || (next != null && (next.StartTime != current.StartTime || next.EndTime != current.EndTime)))
+                {
+                    workignString += " " + current.StartTime.ToString12HourTime() + "-" + current.EndTime.ToString12HourTime() + ", ";
+                }
+            }
+
+            workignString = workignString.Trim().TrimEnd(',');
+
+            return workignString;
+        }
+
+        private static string GetDayOfWeekAbbreviation(DayOfWeek dayOfWeek)
+        {
+            if (dayOfWeek == DayOfWeek.Tuesday || dayOfWeek == DayOfWeek.Thursday)
+            {
+                return dayOfWeek.ToString().Substring(0, 2);
+            }
+
+            return dayOfWeek.ToString().Substring(0, 1);
+        }
     }
 }
--- Smart/Data/Models/ScheduleAvailability.cs
+++ Smart/Data/Models/ScheduleAvailability.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Smart.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace Smart.Data.Models
 {
