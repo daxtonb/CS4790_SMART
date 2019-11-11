058d7c8182f6e65f3eccc19fa1cf507cff11c04b
Austin <austinwilcox1@mail.weber.edu>
2019-11-10 23:46:19 -0700

    Made lots of changes to scheduling. I am trying to keep everything strictly to one page, and that should make all operations a bit easier. I'm still trying to figure out a good way to sign up for required classes. But I ahve made it a lot easier to register and see all courses that are avaialable. Currently I am not filtering by term though.

---
+++ Smart/Pages/Scheduling/PublicSchedule.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Smart.Data;
+using Smart.Data.Models;
+
+namespace Smart.Pages.Scheduling
+{
+    public class PublicScheduleModel : PageModel
+    {
+        public List<StudentPublicSchoolClass> PublicSchedule { get; set; }
+        private readonly ApplicationDbContext _db;
+
+        public PublicScheduleModel(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+
+        public async Task OnGetAsync(int? studentId)
+        {
+            if(studentId == null)
+            {
+                PublicSchedule = await _db.StudentPublicSchoolClasss.Where(s => s.StudentId == studentId).ToListAsync();
+            }
+            else
+            {
+                NotFound();
+            }
+        }
+    }
+}
