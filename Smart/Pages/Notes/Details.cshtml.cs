32381c52acf0c00d95b8deaf41f2582ba917ed6a
juanrangeljuarez <juanrangel1@mail.weber.edu>
2019-11-10 18:16:36 -0700

    notes

---
+++ Smart/Pages/Notes/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Smart.Data;
+using Smart.Data.Models;
+
+namespace Smart.Pages.Notes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Smart.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Smart.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Note Note { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Note = await _context.Notes
+                .Include(n => n.NoteType)
+                .Include(n => n.Student)
+                .Include(n => n.User).FirstOrDefaultAsync(m => m.NoteId == id);
+
+            if (Note == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Smart/Pages/Notes/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Smart.Data;
+using Smart.Data.Models;
+
+namespace Smart.Pages.Notes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Smart.Data.ApplicationDbContext _context;
+
+       
+
+        public IndexModel(Smart.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Note> Note { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Note = await _context.Notes
+                .Include(n => n.NoteType)
+                .Include(n => n.Student)
+                .Include(n => n.User).ToListAsync();
+        }
+    }
+}
