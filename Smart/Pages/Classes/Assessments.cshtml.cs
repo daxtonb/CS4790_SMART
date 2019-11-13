e8e43edfbad540b532c98fc5fedb8d08ceb1625a
Daxton Burgon <dax@dentma.com>
2019-11-12 20:28:50 -0700

    Re-arranged form modal to be global in the _Layout.cshtml file. Added a script to support this. Added Assessment form modal.

--- Smart/Pages/Classes/Assessments.cshtml.cs
+++ Smart/Pages/Classes/Assessments.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
 using Smart.Data;
 using Smart.Data.Models;
@@ -30,12 +31,38 @@ namespace Smart.Pages.Classes
                 .Include(c => c.ClassSchedules).ThenInclude(c => c.ScheduleAvailability)
                 .FirstOrDefaultAsync(c => c.ClassId == classId);
 
-            Assessments = @class.Assessments;
+            Assessments = @class.Assessments.OrderByDescending(a => a.Deadline);
 
             // For layout
             ViewData["ClassTitle"] = $"{@class.Course.Name} - {@class.Term.TimeOfYear} {@class.Term.StartDate.Year}";
             ViewData["ClassSubtitle"] = ClassSchedule.GetScheduleString(@class.ClassSchedules.OrderBy(c => c.ScheduleAvailability.DayOfWeek));
             ViewData["ClassId"] = @class.ClassId;
+        }
+
+        public async Task<IActionResult> OnGetAssessmentFormAsync(int classId, int? assessmentId)
+        {
+            Assessment assessment;
+
+            if (assessmentId.HasValue)
+            {
+                assessment = await _context.Assessment.FirstOrDefaultAsync(a => a.AssessmentId == assessmentId.Value);
+
+                if (assessment == null)
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                assessment = new Assessment() { ClassId = classId, Deadline = DateTime.Today.AddDays(1) };
+            }
+
+
+            return new PartialViewResult()
+            {
+                ViewName = "_AssessmentForm",
+                ViewData = new ViewDataDictionary<Assessment>(ViewData, assessment)
+            };
         }
     }
 }
