32383f6fd82d9f1bf82fce61ce2872783fd75abe
Daxton Burgon <dax@dentma.com>
2019-11-12 20:28:01 -0700

    Updated Assessment to be of date type Date (instead of DateTime)

--- Smart/Data/Models/Assessment.cs
+++ Smart/Data/Models/Assessment.cs
@@ -17,6 +17,8 @@ namespace Smart.Data.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public int PointsPossible { get; set; }
+        [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
         public DateTime Deadline { get; set; }
 
         public virtual Class Class { get; set; }
