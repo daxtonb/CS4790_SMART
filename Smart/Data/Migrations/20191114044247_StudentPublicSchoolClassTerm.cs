b9a4e71b25016391df1e5fc02915209e7e4b644d
Daxton Burgon <dax@dentma.com>
2019-11-13 21:43:56 -0700

    Added term to student public schools schedule.

---
+++ Smart/Data/Migrations/20191114044247_StudentPublicSchoolClassTerm.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Smart.data.migrations
+{
+    public partial class StudentPublicSchoolClassTerm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TermId",
+                table: "StudentPublicSchoolClass",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentPublicSchoolClass_TermId",
+                table: "StudentPublicSchoolClass",
+                column: "TermId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentPublicSchoolClass_Term_TermId",
+                table: "StudentPublicSchoolClass",
+                column: "TermId",
+                principalTable: "Term",
+                principalColumn: "TermId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentPublicSchoolClass_Term_TermId",
+                table: "StudentPublicSchoolClass");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudentPublicSchoolClass_TermId",
+                table: "StudentPublicSchoolClass");
+
+            migrationBuilder.DropColumn(
+                name: "TermId",
+                table: "StudentPublicSchoolClass");
+        }
+    }
+}
--- Smart/Data/Models/StudentPublicSchoolClass.cs
+++ Smart/Data/Models/StudentPublicSchoolClass.cs
@@ -14,8 +14,10 @@ namespace Smart.Data.Models
         [MaxLength(128)]
         public string CourseName { get; set; }
         public TimeOfYear TimeOfYear { get; set; }
+        public int TermId { get; set; }
 
         public virtual ICollection<PublicSchoolClassSchedule> PublicSchoolClassSchedules { get; set; }
         public virtual Student Student { get; set; }
+        public virtual Term Term { get; set; }
     }
 }
