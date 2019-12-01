75df6a3fd679bd70907325e9edaafb0fd4c4c432
Austin <austinwilcox1@mail.weber.edu>
2019-12-01 15:10:52 -0700

    Updated the Front end for my pages, The front end is basically finished out, It has added some of the modifications that Dr. Fry wanted. Now I just need to wire up the backend and get everything up and functioning.

--- Smart/Pages/Scheduling/Index.cshtml.cs
+++ Smart/Pages/Scheduling/Index.cshtml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -18,148 +18,27 @@ namespace Smart.Pages.Scheduling
     {
         private readonly ApplicationDbContext _db;
         public Student MyStudent { get; set; }
-        public List<StudentClass> MyClasses { get; set; }
-        public List<Course> RequiredCourses { get; set; }
-        public List<Class> Classes { get; set; }
-        public List<ScheduleAvailability> ScheduleAvailabilities { get; set; }
-        public List<PublicScheduleVM> MyPublicSchoolSchedule { get; set; }
-        public List<StudentPublicSchoolClass> PublicSchedule { get; set; }
-
+        public Class Classes { get; set; }
+        public IEnumerable<Term> Terms { get; set; }
 
         public IndexModel(ApplicationDbContext dbContext)
         {
             _db = dbContext;
-            ScheduleAvailabilities = new List<ScheduleAvailability>();
-            MyPublicSchoolSchedule = new List<PublicScheduleVM>();
         }
 
         public async Task OnGetAsync(int? studentId)
         {
-            Classes = await _db.Classes
-                               .Include(i => i.ClassSchedules)
-                               .Include(i => i.InstructorUser)
-                               .Include(i => i.StudentClasses)
-                               .OrderBy(i => i.Course.Name)
-                               //.Where(i => !i.StudentClasses.Any(s => s.StudentId == 3))
-                               .ToListAsync();
-
-            foreach (Class c in Classes)
+            if(studentId == null)
             {
-                foreach (ClassSchedule cs in c.ClassSchedules)
-                {
-                    ScheduleAvailabilities.AddRange(_db.ScheduleAvailabilities
-                                                       .Where(i => i.ScheduleAvailabilityId == cs.ScheduleAvailabilityId)
-                                                       .ToList());
-                }
-            }
-
-            RequiredCourses = await _db.Courses
-                                       .Where(c => c.IsCoreRequirement)
-                                       .ToListAsync();
-            
-
-            if (studentId == null)
-            {
-                MyStudent = _db.Students.Include(c => c.StudentClasses)
-                                       .FirstOrDefault(i => i.StudentId == 3);
-                MyClasses = await _db.StudentClasses.Include(c => c.Class)
-                                                    //.ThenInclude(s => s.ClassSchedules)
-                                                    .Where(c => c.StudentId == 3)
-                                                    .ToListAsync();
-
-                PublicSchedule = await _db.StudentPublicSchoolClasss
-                                         .Where(s => s.StudentId == 3)
-                                         .Include(s => s.PublicSchoolClassSchedules)
-                                         .ToListAsync();
-
-                foreach (var schoolClass in PublicSchedule)
-                {
-                    MyPublicSchoolSchedule.Add(new PublicScheduleVM((int)3, schoolClass.TermId, schoolClass.StudentPublicSchoolClassId, _db));
-                }
+                Redirect("../Index");
             }
             else
             {
-                MyStudent = _db.Students.Include(c => c.StudentClasses)
-                                        .FirstOrDefault(i => i.StudentId == studentId);
-                MyClasses = await _db.StudentClasses.Include(c => c.Class)
-                                                    .Where(c => c.StudentId == studentId)
-                                                    .ToListAsync();
-
-                PublicSchedule = await _db.StudentPublicSchoolClasss
-                                         .Where(s => s.StudentId == studentId)
-                                         .Include(s => s.PublicSchoolClassSchedules)
-                                         .ToListAsync();
-
-                foreach (var schoolClass in PublicSchedule)
-                {
-                    MyPublicSchoolSchedule.Add(new PublicScheduleVM((int)studentId, schoolClass.TermId, schoolClass.StudentPublicSchoolClassId, _db));
-                }
+                MyStudent = await _db.Students.FindAsync(studentId);
+                Terms = await _db.Terms.ToListAsync();
             }
+
+
         }
-        public async Task<IActionResult> OnPostAsync(int id, int studentId)
-        {
-            //Count is greater than 0 meaning that there are elements found within
-            if(Request.Form["RemoveClass"].Count > 0)
-            {
-                var classToRemove = Request.Form["classId"][0];
-                var myStudentId = Request.Form["StudentId"][0];
-
-                if (Request.Form["IsPublicSchool"][0].ToString().Equals("1"))
-                {
-                    //This is public school and I want to erase that link
-                    var tempPublicClass = _db.StudentPublicSchoolClasss.Where(i => i.StudentPublicSchoolClassId == int.Parse(classToRemove)).FirstOrDefault();
-
-                    if(tempPublicClass != null)
-                    {
-                        _db.StudentPublicSchoolClasss.Remove(tempPublicClass);
-                        await _db.SaveChangesAsync();
-                    }
-                }
-                else
-                {
-                    var tempClass = _db.StudentClasses.Where(i => i.ClassId == int.Parse(classToRemove) && int.Parse(myStudentId) == i.StudentId).FirstOrDefault();
-
-                    if (tempClass != null)
-                    {
-                        _db.StudentClasses.Remove(tempClass);
-                        await _db.SaveChangesAsync();
-                    }
-                }
-                return RedirectToPage("Index", new { studentId });
-            }
-            else
-            {
-                try
-                {
-                    if (id != null)
-                    {
-                        StudentClass myClass = new StudentClass() { ClassId = id, StudentId = studentId };
-
-                        //var classCheck = _db.StudentClasses.Where(i => i.ClassId == id && i.StudentId == studentId);
-                        //if(classCheck == null)
-                        //{
-                        _db.StudentClasses.Add(myClass);
-                        await _db.SaveChangesAsync();
-
-                        return RedirectToPage("Index", new { studentId });
-                        //}
-                        //else
-                        //{
-                        //    return RedirectToPage();
-                        //}
-
-                    }
-                }
-                catch(Exception)
-                {
-                    return NotFound();
-                }
-            }
-
-            //This right now means that there is an error
-            return NotFound();
-        }
-
-
     }
 }
--- Smart/Pages/Scheduling/PublicSchedule.cshtml.cs
+++ Smart/Pages/Scheduling/PublicSchedule.cshtml.cs
@@ -1,224 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Smart.Data;
-using Smart.Data.Models;
 
 namespace Smart.Pages.Scheduling
 {
     public class PublicScheduleModel : PageModel
     {
-        public List<StudentPublicSchoolClass> PublicSchedule { get; set; }
-        private readonly ApplicationDbContext _db;
-        public List<PublicScheduleVM> MyPublicSchoolSchedule { get; set; }
-
-
-        [BindProperty]
-        public int StudentID { get; set; }
-
-        [BindProperty]
-        [DisplayName("Course Name")]
+        public int StudentID { get; set; } = 3;
         public string CourseName { get; set; }
-        [BindProperty]
-        [DisplayName("Day of The Week")]
-        public string DayOfWeek { get; set; }
-        [BindProperty]
-        [DisplayName("Start Time")]
-        public DateTime StartTime { get; set; }
-        [BindProperty]
-        [DisplayName("End Time")]
-        public DateTime EndTime { get; set; }
-
-
-        public PublicScheduleModel(ApplicationDbContext context)
-        {
-            _db = context;
-            MyPublicSchoolSchedule = new List<PublicScheduleVM>();
-        }
-
-        public async Task OnGetAsync(int? studentId)
-        {
-            if(studentId != null)
-            {
-                this.StudentID = (int)studentId;
-                PublicSchedule = await _db.StudentPublicSchoolClasss
-                                          .Where(s => s.StudentId == studentId)
-                                          .Include(s => s.PublicSchoolClassSchedules)
-                                          .ToListAsync();
-
-                foreach(var schoolClass in PublicSchedule)
-                {
-                    MyPublicSchoolSchedule.Add(new PublicScheduleVM((int)studentId, schoolClass.TermId, schoolClass.StudentPublicSchoolClassId, _db));
-                }
-            }
-            else
-            {
-                NotFound();
-            }
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if(Request.Form["RemoveClass"].Count > 0)
-            {
-                var classToRemove = Request.Form["classId"][0];
-                var myStudentId = Request.Form["StudentId"][0];
-
-                var tempPublicClass = _db.StudentPublicSchoolClasss.Where(i => i.StudentPublicSchoolClassId == int.Parse(classToRemove)).FirstOrDefault();
-
-                if (tempPublicClass != null)
-                {
-                    _db.StudentPublicSchoolClasss.Remove(tempPublicClass);
-                    await _db.SaveChangesAsync();
-                }
-            }
-            else
-            {
-                if (!ModelState.IsValid)
-                {
-                    return Page();
-                }
-
-                //The values as they are saved in the database
-                var monday = Request.Form["Monday"]; //1
-                var tuesday = Request.Form["Tuesday"]; //2
-                var wednesday = Request.Form["Wednesday"]; //3
-                var thursday = Request.Form["Thursday"]; //4
-                var friday = Request.Form["Friday"]; //5
-
-                List<ScheduleAvailability> days = new List<ScheduleAvailability>();
-                if (monday.Count > 0)
-                {
-                    days.Add(new ScheduleAvailability()
-                    {
-                        DayOfWeek = System.DayOfWeek.Monday,
-                        StartTime = StartTime.TimeOfDay,
-                        EndTime = EndTime.TimeOfDay
-                    });
-                }
-                if (tuesday.Count > 0)
-                {
-                    days.Add(new ScheduleAvailability()
-                    {
-                        DayOfWeek = System.DayOfWeek.Tuesday,
-                        StartTime = StartTime.TimeOfDay,
-                        EndTime = EndTime.TimeOfDay
-                    });
-                }
-                if (wednesday.Count > 0)
-                {
-                    days.Add(new ScheduleAvailability()
-                    {
-                        DayOfWeek = System.DayOfWeek.Wednesday,
-                        StartTime = StartTime.TimeOfDay,
-                        EndTime = EndTime.TimeOfDay
-                    });
-                }
-                if (thursday.Count > 0)
-                {
-                    days.Add(new ScheduleAvailability()
-                    {
-                        DayOfWeek = System.DayOfWeek.Thursday,
-                        StartTime = StartTime.TimeOfDay,
-                        EndTime = EndTime.TimeOfDay
-                    });
-                }
-                if (friday.Count > 0)
-                {
-                    days.Add(new ScheduleAvailability()
-                    {
-                        DayOfWeek = System.DayOfWeek.Friday,
-                        StartTime = StartTime.TimeOfDay,
-                        EndTime = EndTime.TimeOfDay
-                    });
-                }
-
-                Term myTerm = _db.Terms.Where(i => i.StartDate < DateTime.Now && i.EndDate > DateTime.Now).FirstOrDefault();
-
-                StudentPublicSchoolClass myClass = new StudentPublicSchoolClass()
-                {
-                    CourseName = this.CourseName,
-                    StudentId = this.StudentID,
-                    PublicSchoolClassSchedules = days.Select(i => new PublicSchoolClassSchedule()
-                    {
-                        ScheduleAvailabilityd = i
-                    }).ToList(),
-                    TermId = myTerm.TermId
-                };
-
-                _db.StudentPublicSchoolClasss.Add(myClass);
-                await _db.SaveChangesAsync();
-            }
-            
-            return RedirectToPage("PublicSchedule", new { this.StudentID });
-        }
-
-
-    }
-
-    public class PublicScheduleVM
-    {
-        public int StudentID { get; set; }
-        public string CourseName { get; set; }
-        public int TermId { get; set; }
-        public int ClassId { get; set; }
-        public string DaysOfTheWeek { get; set; } = "";
         public string StartTime { get; set; }
         public string EndTime { get; set; }
-        public List<ScheduleAvailability> Schedule { get; set; }
-        private readonly ApplicationDbContext _db;
-
-        public PublicScheduleVM(int studentId, int termId, int classId, ApplicationDbContext context)
+        public string DayOfWeek { get; set; }
+        public void OnGet()
         {
-            this.StudentID = studentId;
-            this.TermId = termId;
-            this.ClassId = classId;
-            _db = context;
-            Schedule = new List<ScheduleAvailability>();
 
-            getSchedule();
-        }
-
-        private void getSchedule()
-        {
-            using (SqlConnection conn = new SqlConnection("Server=titan.cs.weber.edu,10433;Database=AustinsFinalProjectTestDatabase;user id=4790;password=4790$tudent"))
-            {
-                conn.Open();
-
-                using (SqlCommand cmd = new SqlCommand("SELECT CASE " +
-                                                       "WHEN DayOfWeek = 1 THEN 'M' " +
-                                                       "WHEN DayOfWeek = 2 THEN 'Tu' " +
-                                                       "WHEN DayOfWeek = 3 THEN 'W' " +
-                                                       "WHEN DayOfWeek = 4 THEN 'Th' " +
-                                                       "WHEN DayOfWeek = 5 THEN 'F' " +
-                                                       "END AS [CourseDayOfWeek], StartTime, EndTime, CourseName FROM StudentPublicSchoolClass c INNER JOIN PublicSchoolClassSchedule s ON s.StudentPublicSchoolClassId = c.StudentPublicSchoolClassId INNER JOIN ScheduleAvailability a ON a.ScheduleAvailabilityId = s.ScheduleAvailabilityId INNER JOIN Term t ON t.TermId = c.TermId WHERE StudentId = @StudentID AND c.TermId = @TermId AND c.StudentPublicSchoolClassId = @ClassId", conn))
-                {
-                    cmd.Parameters.Add("@StudentId", SqlDbType.Int).Value = this.StudentID;
-                    cmd.Parameters.Add("@TermId", SqlDbType.Int).Value = this.TermId;
-                    cmd.Parameters.Add("@ClassId", SqlDbType.Int).Value = this.ClassId;
-
-                    using (SqlDataReader rd = cmd.ExecuteReader())
-                    {
-                        while(rd.Read())
-                        {
-                            CourseName = rd["CourseName"].ToString();
-                            StartTime = rd["StartTime"].ToString();
-                            EndTime = rd["EndTime"].ToString();
-                            DaysOfTheWeek += rd["CourseDayOfWeek"].ToString();
-                        }
-                        rd.Close();
-                    }
-                }
-
-                conn.Close();
-            }
         }
     }
 }
