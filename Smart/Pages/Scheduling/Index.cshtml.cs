2a45f1b196e3b8519bea41cae21e478eaaa05a24
Austin Wilcox <austin21wilcox@gmail.com>
2019-12-07 19:16:11 -0700

    Code works now on my branch, just need to do a merge with master 2.0

--- Smart/Pages/Scheduling/Index.cshtml.cs
+++ Smart/Pages/Scheduling/Index.cshtml.cs
@@ -22,45 +22,53 @@ namespace Smart.Pages.Scheduling
         public List<Meeting> Meetings {get; set;}
         public List<School> PublicSchools {get; set;}
         public School SmartSchool {get; set;}
+        public Term CurrentTerm {get; set;}
+        public int TermId {get; set;}
 
         public IndexModel(ApplicationDbContext dbContext)
         {
             _db = dbContext;
         }
 
-        public async Task OnGetAsync(int studentId)
+        public async Task OnGetAsync(int studentId, int? termId)
         {
+            CurrentTerm = await _db.Terms.Where(i => i.StartDate < DateTime.Now && i.EndDate > DateTime.Now).FirstOrDefaultAsync();
+
+            if(termId == null)
+            {
+                TermId = CurrentTerm.TermId;
+            }
+            else{
+                TermId = (int)termId;
+            }
+            Console.WriteLine("Reached this function");
             MyStudent = await _db.Students.FindAsync(studentId);
             Terms = await _db.Terms.ToListAsync();
             PublicSchools = await _db.Schools.Where(i => !(i.Name.Contains("No Poor Among Us"))).ToListAsync();
             SmartSchool = await _db.Schools.Where(i => i.Name.Contains("No Poor Among Us")).FirstOrDefaultAsync();
 
-            StudentMeetings = await _db.StudentMeetings
-                                        .Include(i => i.Meeting)
-                                        .ThenInclude(i => i.Class)
-                                        .ThenInclude(i => i.Course)
-                                        .ThenInclude(i => i.School)
-                                        .ThenInclude(i => i.ScheduleAvailabilities)
-                                        .Include(i => i.Meeting)
-                                        .ThenInclude(i => i.Class)
-                                        .ThenInclude(i => i.InstructorUser)
-                                        .Where(i => i.StudentId == studentId)
-                                        .ToListAsync();
-
-            // Meetings = await _db.Meetings.Include(i => i.Class)
-            //                         .ThenInclude(c => c.Course)
-            //                         .ThenInclude(s => s.School)
-            //                         .ThenInclude(i => i.ScheduleAvailabilities).ToListAsync();
             Meetings = await _db.Meetings.Include(i => i.Class)
-                                            .ThenInclude(c => c.Course)
-                                            .ThenInclude(c => c.School)
-                                            .ThenInclude(i => i.ScheduleAvailabilities)
-                                            .Include(i => i.Class)
-                                            .ThenInclude(i => i.InstructorUser)
-                                            .Include(i => i.Class)
-                                            .ThenInclude(i => i.Term)
-                                            .Include(i => i.StudentMeetings)
-                                            .ToListAsync();
+                                                            .ThenInclude(c => c.Course)
+                                                            .ThenInclude(c => c.School)
+                                                            .ThenInclude(i => i.ScheduleAvailabilities)
+                                                            .Include(i => i.Class)
+                                                            .ThenInclude(i => i.InstructorUser)
+                                                            .Include(i => i.Class)
+                                                            .ThenInclude(i => i.Term)
+                                                            .Include(i => i.StudentMeetings)
+                                                            .Where(i => i.Class.TermId == TermId)
+                                                            .ToListAsync();
+                StudentMeetings = await _db.StudentMeetings
+                                                .Include(i => i.Meeting)
+                                                .ThenInclude(i => i.Class)
+                                                .ThenInclude(i => i.Course)
+                                                .ThenInclude(i => i.School)
+                                                .ThenInclude(i => i.ScheduleAvailabilities)
+                                                .Include(i => i.Meeting)
+                                                .ThenInclude(i => i.Class)
+                                                .ThenInclude(i => i.InstructorUser)
+                                                .Where(i => i.StudentId == studentId && i.Meeting.Class.TermId == TermId)
+                                                .ToListAsync();
 
             foreach(var meeting in StudentMeetings)
             {
@@ -76,7 +84,14 @@ namespace Smart.Pages.Scheduling
         public async Task<IActionResult> OnPostAsync(int meetingId, int studentId, int? delete)
         {
             Console.WriteLine("Posting Request Method: " + Request.Method);
+            var value = Request.Form["TermId"];
 
+            if(!String.IsNullOrEmpty(value.ToString()))
+            {
+                int termId = int.Parse(value.ToString());
+                
+                return RedirectToPage(new {studentId = studentId, termId = termId});
+            }
             if(delete != null)
             {
                 Console.WriteLine("Reached Delete Function");
@@ -110,4 +125,30 @@ namespace Smart.Pages.Scheduling
             return RedirectToPage(new {studentId = studentId});     
         }
     }
+
+    /*
+<!-- <div class="col-4 text-center">
+
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" id="PublicSchoolDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Public Schools
+                    </button>
+                    
+                        <div class="dropdown-menu" aria-labelledby="PublicSchoolDropdown">
+                        @foreach(var school in @Model.PublicSchools)
+                        {
+                            <button class="dropdown-item" onclick="document.getElementById('PublicSchoolSelected').innerHTML = '<h2>@school.Name</h2>'; document.getElementById('SelectedPublicSchoolForPotentialClasses').innerText = '@school.Name'; document.getElementById('SelectedPublicSchool').value = @school.SchoolId;">@school.Name</button>
+                        }
+                        <button class="dropdown-item" onclick="document.getElementById('PublicSchoolSelected').innerHTML = '<h2>School 1</h2>'; document.getElementById('SelectedPublicSchoolForPotentialClasses').innerText = 'School 1'; document.getElementById('SelectedPublicSchool').value = 1;">School 1</button>
+                        <button class="dropdown-item" onclick="document.getElementById('PublicSchoolSelected').innerHTML = '<h2>School 2</h2>'; document.getElementById('SelectedPublicSchoolForPotentialClasses').innerText = 'School 2'; document.getElementById('SelectedPublicSchool').value = 2;">School 2</button>
+                        <button class="dropdown-item" onclick="document.getElementById('PublicSchoolSelected').innerHTML = '<h2>School 3</h2>'; document.getElementById('SelectedPublicSchoolForPotentialClasses').innerText = 'School 3'; document.getElementById('SelectedPublicSchool').value = 3;">School 3</button> -->
+                    </div> 
+                    <input type="hidden" id="SelectedPublicSchool" />
+                </div> 
+                
+            </div> -->
+
+
+*/
 }
+
